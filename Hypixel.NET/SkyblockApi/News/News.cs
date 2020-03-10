15e35dfa131c876676d4b21648e9ec042ac34bf1
Artu165 <arturojaredmillan@yahoo.com.mx>
2020-03-09 23:29:31 -0600

    Added News API

---
+++ Hypixel.NET/SkyblockApi/News/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Hypixel.NET.SkyblockApi.News
+{
+    public class News
+    {
+        [JsonProperty("link")]
+        public string Link { get; private set; }
+        [JsonProperty("text")]
+        public string Text { get; private set; }
+        [JsonProperty("title")]
+        public string Title { get; private set; }
+    }
+}
